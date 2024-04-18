87d54e6ea5bb790ec846c9715ca148ed8baeb602
j2noo <wlsdndml213@gmail.com>
2024-04-18 19:05:34 +0900

    feat : 로고 추가

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 const Main = () => {
 
   return (
-   <MainLayout><div>메인 페이지ss</div></MainLayout>
+   <MainLayout><div>메인 페이지ss
+    <div>로고</div></div></MainLayout>
   );
 };
 export default Main;
